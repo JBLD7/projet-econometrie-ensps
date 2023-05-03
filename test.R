39ec56693e97a62f929e953ead30f1db679a320b
JBLD7 <jean-baptiste.lagrange@ens-paris-saclay.fr>
2023-05-03 10:30:34 +0200

    v7 j'ai ajouté quelques commandes pour récupérer des variables spécifiques ... pas sûr que ça fonctionne.

--- test.R
+++ test.R
@@ -1,10 +1,10 @@
 #Code - Économétrie
 
-#à exécuter en premier pour installer les packages
+###à exécuter en premier pour installer les packages
 install.packages('psidR')
 install.packages("data.table")
 
-#Pour créer le data frame
+###Pour créer le data frame
 library(psidR)
 library(data.table)
 r = system.file(package="psidR")
@@ -14,9 +14,30 @@ i = dcast(i[,list(year,name,variable)],year~name, value.var = "variable")
 f = dcast(f[,list(year,name,variable)],year~name, value.var = "variable")
 d = build.panel(datadir="~/projet-econometrie-ensps/data/PSID/",fam.vars=f,ind.vars=i, heads.only = TRUE,sample="SRC",design="all")
 
-#Pour récupérer des variables spécifiques 
-#Je mettrai le code plus tard.
+###Pour récupérer des variables spécifiques 
+library(openxlsx)
+library(psidR)
+library(data.table)
+r = system.file(package="psidR")
+f = fread(file.path(r,"psid-lists","famvars.txt"))
+i = fread(file.path(r,"psid-lists","indvars.txt"))
+cwf <- read.xlsx("http://psidonline.isr.umich.edu/help/xyr/psid.xlsx")
+#getNamesPSID("ER75812", cwf, years = 2017) #remplacer par le nom de la variable ! 
+getNamesPSID("ER17013", cwf, years = c("2003", "2005", "2007", "2009")) #some years
+#getNamesPSID("ER17013", cwf, years = NULL)  # all years
+# next, bring into required shape:
+i = dcast(i[,list(year,name,variable)],year~name, value.var = "variable")
+f = dcast(f[,list(year,name,variable)],year~name, value.var = "variable")
+# call the builder function
+e = build.panel(datadir="~/projet-econometrie-ensps/data/PSID/",fam.vars=f,ind.vars=i, heads.only = TRUE,sample="SRC",design="all")
+# d contains your panel
+save(e,file="~/psid.Rds")
 
+cwf = openxlsx::read.xlsx(system.file(package="psidR","psid-lists","psid.xlsx"))
+head_age_var_name <- getNamesPSID("ER17013", cwf, years=c(2003))
+famvars = data.frame(year=c(2003),age=head_age_var_name)
+build.panel(fam.vars=famvars,datadir="~/projet-econometrie-ensps/data/PSID/")
+#Je n'ai pas tout à fait bien compris comment ça marchait. Je mets la documentation du package dans les fichiers du projet. 
 
 
 
